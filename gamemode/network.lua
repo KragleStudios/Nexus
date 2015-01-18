6be12e1977d32941a5dd3e2fb19cf33237285396
arcaneex <epic@funny.cat>
2015-01-18 05:53:27 +0200

    Fixed network library to work on client

--- gamemode/network.lua
+++ gamemode/network.lua
@@ -2,19 +2,28 @@ network = {}
 network.__index = network
 
 local nets = {}
+nets['__newNet'] = {}
+
+if SERVER then
+	util.AddNetworkString('__newNet')
+end
 
 function network.new(sName)
 	if !nets[sName] then
 		if SERVER then
 			util.AddNetworkString(sName)
+		else
+			network.new('__newNet'):write(sName):send()
 		end
-		nets[sName] = {}
 	end
+
+	nets[sName] = {}
 	nets[sName].stream = {}
 	nets[sName].name = sName
 	setmetatable(nets[sName],network)
 	return nets[sName]
 end
+
 function network.reader(sName)
 	return setmetatable({name = sName},network)
 end
@@ -25,11 +34,28 @@ function network:write(content,bits)
 	table.insert(self.stream,{content = content, bits = bits})
 	return self
 end
+
+function network:pooled()
+	return util.NetworkStringToID(self.name) ~= 0
+end
+
+function network:wait(callback,...)
+	timer.Simple(.1,function()
+		if not self:pooled() then self:wait() else callback() end
+	end)
+end
+
 function network:send(to)
 	local send = net.Send
 	if !to and CLIENT then
 		send = net.SendToServer
 	end
+
+	if not self:pooled() then
+		self:wait(function() self:send(to) end)
+		return
+	end
+
 	net.Start(self.name)
 	for i=1,#self.stream do
 		local obj = self.stream[i]['content']
@@ -58,5 +84,7 @@ function network:send(to)
 	end
 	send(to)
 end
+
+if SERVER then network.reader('__newNet'):listen(function() util.AddNetworkString(net.ReadString()) end) end
 
 debug.getregistry().Network = network
