3267ef6438c8328fa5b7bddb4ac16ce631f9ad95
arcaneex <epic@funny.cat>
2015-01-18 03:48:12 +0200

    Made avatar use skin

--- gamemode/vgui/avatar.lua
+++ gamemode/vgui/avatar.lua
@@ -1,4 +1,4 @@
-local pnl = newPanel('NAvatar','NPanel')
+local pnl = newPanel('NAvatar','DPanel')
 
 function pnl:Init()
 	self:SetSize(80,80)
@@ -6,10 +6,11 @@ function pnl:Init()
 	self.avatar:SetPos(4,4)
 	self.avatar:SetPlayer(LocalPlayer(),64)
 	self.avatar:SetSize(72,72)
+	self.skin = derma.GetNamedSkin('Nexus')
 end
 
 function pnl:Paint(w,h)
-	draw.RoundedBox(8,0,0,w,h,self.oCol)
+	draw.RoundedBox(8,0,0,w,h,self.skin.FrameBorderColor)
 end
 
 pnl:registerPanel()
